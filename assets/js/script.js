149933f4ad9d5b84072225b21684cb763d7b9b29
j-faust <85812553+j-faust@users.noreply.github.com>
2022-11-05 10:47:14 -0400

    Added functionality to update weather to correct container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 let weatherUrlApi = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=864118318ed70a271edc09feffa72f3d';
 let searchBtn = document.getElementById('search-btn');
 let searchBox = document.getElementById('city-search');
-let currentWeatherBox = document.getElementById('current-weather');
+let cityName = document.getElementById('city-name');
+let currentId = document.getElementById('current-id');
+let currentConditions = document.getElementById('current-conditions');
+let currentIcon = document.getElementById('current-icon');
+let currentMain = document.getElementById('current-main');
+let currentDesc = document.getElementById('current-desc');
+let currentHumidity = document.getElementById('current-humid');
+let currentTemperature = document.getElementById('current-temp');
+let currentWindSpeed = document.getElementById('current-wind');
 let forecastWeatherBox = document.getElementById('forecast-weather');
 
 
@@ -11,26 +19,55 @@ function weatherSearch(city) {
     let url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=864118318ed70a271edc09feffa72f3d';
     let forecast = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&units=imperial&appid=864118318ed70a271edc09feffa72f3d';
 
+
     // Fetching the current weather
     fetch(url)
-    .then(function(response) {
-        console.log(response.status);
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        // Loading weather data to the browser for current weather
+        cityName.innerHTML = data.name;
+        currentIcon.innerHTML = data.weather.icon;
+        currentId.innerHTML = data.weather.id;
+        currentMain.innerHTML = data.weather.main;
+        currentDesc.innerHTML = "<strong>Current Weather:</strong> " + data.weather.description;
+        currentHumidity.innerHTML = "<strong>Current Humidity:</strong " + data.main.humidity;
+        currentTemperature.innerHTML = "<strong>Current Temperature:</strong> " + data.main.temp;
+        currentWindSpeed.innerHTML = "<strong>Current Wind Speed:</strong " + data.wind.speed;
     })
 
-    // fetching the forecast weather
+    // logging any errors to the console
+    .catch(console.error);  
+
+    // fetching the forecast weather 
     fetch(forecast)
-        .then(function(response){
-            console.log(response.status)
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        // Loading weather data to the web browser for forecast weather
+        // forecastWeatherBox.innerHTML = data.name;
+        // forecastWeatherBox.innerHTML = data.weather.id;
+        // forecastWeatherBox.innerHTML = data.weather.main;
+        // forecastWeatherBox.innerHTML = data.weather.description;
+        // forecastWeatherBox.innerHTML = data.weather.icon;
+        // forecastWeatherBox.innerHTML = data.main.humidity;
+        // forecastWeatherBox.innerHTML = data.main.temp;
+        // forecastWeatherBox.innerHTML = data.wind.speed;
+        for(var i = 1; i <= 5; i++){
+            let elName = "forecast" + i.toString();
+            console.log(elName);
+           let day = document.getElementById('forecast' + i.toString()); 
+           console.log(day);
+           day.textContent = "day" + i.toString();
+
+        }
+
     })
+    // log any error that would occur during fetch to the console
+    .catch(console.error);
 }
+
+
 
 // Function to call the weatherSeach function to get a value for the search box
 function buttonClick() {
@@ -39,3 +76,7 @@ function buttonClick() {
 
 // Event listener to call the function buttonClick to get the required data
 searchBtn.addEventListener('click', buttonClick);
+
+
+
+
